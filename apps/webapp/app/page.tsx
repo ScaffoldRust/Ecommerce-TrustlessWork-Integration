892e6dbfa308de7573cc162a937402351237e448
Sendi John <johnsendi727@gmail.com>
2025-07-04 18:39:18 +0100

    Add responsive How It Works section component

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/home/hero-section";
 import { WhyChoose } from "@/components/home/why-choose";
 import CtaSection from "@/components/home/cta-section";
+import HowItWorks from '@/components/home/how-it-works';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <WhyChoose />
+      <HowItWorks />
       <CtaSection />
     </>
   );
