23a89f331a36ed22a183b5abc6a2d1e35765f01c
DevEmmac <eolasunkanmi@gmail.com>
2025-07-06 22:42:00 +0100

    rename hero to herosection

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -1,4 +1,4 @@
-import Hero from "@/components/home/hero-section";
+import HeroSection from "@/components/home/hero-section";
 import { WhyChoose } from "@/components/home/why-choose";
 import CtaSection from "@/components/home/cta-section";
 import HowItWorks from '@/components/home/how-it-works';
@@ -6,7 +6,7 @@ import HowItWorks from '@/components/home/how-it-works';
 export default function Home() {
   return (
     <>
-      <Hero />
+      <HeroSection />
       <WhyChoose />
       <HowItWorks />
       <CtaSection />
